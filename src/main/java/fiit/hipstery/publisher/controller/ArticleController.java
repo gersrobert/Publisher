704f9d4d1f396a232c53a5160c0c60ea5f556946
Robert Gers <rgers7@gmail.com>
2020-03-08 01:00:24 +0100

    Example endpoint

--- src/main/java/fiit/hipstery/publisher/controller/ArticleController.java
+++ src/main/java/fiit/hipstery/publisher/controller/ArticleController.java
@@ -2,12 +2,22 @@ package fiit.hipstery.publisher.controller;
 
 import fiit.hipstery.publisher.bl.service.ArticleService;
 import fiit.hipstery.publisher.dto.ArticleDetailedDTO;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/article")
@@ -16,9 +26,17 @@ public class ArticleController {
     @Autowired
     private ArticleService articleService;
 
+    @Value("classpath:static/loremipsum.txt")
+    private Resource loremIpsum;
+
     @GetMapping("/{id}")
-    public ResponseEntity<ArticleDetailedDTO> getArticleById(@PathVariable String id) {
-        return null;
+    public ResponseEntity<ArticleDetailedDTO> getArticleById(@PathVariable String id) throws IOException {
+        ArticleDetailedDTO articleDetailedDTO = new ArticleDetailedDTO();
+        articleDetailedDTO.setAuthor("Carrie Lam");
+        articleDetailedDTO.setTitle("Bobo je smutny");
+        articleDetailedDTO.setContent(FileUtils.readFileToString(loremIpsum.getFile(), StandardCharsets.UTF_8));
+        articleDetailedDTO.setPublishedAt(LocalDateTime.now());
+        return ResponseEntity.of(Optional.of(articleDetailedDTO));
     }
 
 }
--- src/main/java/fiit/hipstery/publisher/dto/ArticleDetailedDTO.java
+++ src/main/java/fiit/hipstery/publisher/dto/ArticleDetailedDTO.java
@@ -1,4 +1,43 @@
 package fiit.hipstery.publisher.dto;
 
+import java.time.LocalDateTime;
+
 public class ArticleDetailedDTO {
+
+	protected String title;
+	protected String content;
+	protected LocalDateTime publishedAt;
+	protected String author;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public LocalDateTime getPublishedAt() {
+		return publishedAt;
+	}
+
+	public void setPublishedAt(LocalDateTime publishedAt) {
+		this.publishedAt = publishedAt;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 }

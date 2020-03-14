c9cff8c30eedb482df66461094020bd2d3b78498
Cupprum <branisa.samuel@windowslive.com>
2020-03-14 16:54:10 +0100

    started working on insertArticle

--- src/main/java/fiit/hipstery/publisher/bl/impl/ArticleServiceImpl.java
+++ src/main/java/fiit/hipstery/publisher/bl/impl/ArticleServiceImpl.java
@@ -78,11 +78,17 @@ public class ArticleServiceImpl implements ArticleService {
         return resultList.stream().map(this::mapRowToArticleSimpleDTO).collect(Collectors.toList());
     }
 
+    @Override
+    public void insertArticle() {
+        System.out.println("test123");
+    }
+
     private ArticleSimpleDTO mapRowToArticleSimpleDTO(Object[] row) {
         ArticleSimpleDTO articleSimpleDTO = new ArticleSimpleDTO();
         articleSimpleDTO.setTitle((String) row[1]);
         articleSimpleDTO.setPublishedAt(((Timestamp) row[2]).toLocalDateTime());
         articleSimpleDTO.setAuthors(List.of((String) row[3]));
+
         return articleSimpleDTO;
     }
 

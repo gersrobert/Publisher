8a9b39491c5d684a37656c39229f84411fd5eb99
Robert Gers <rgers7@gmail.com>
2020-05-22 14:53:54 +0200

    Javadoc

--- src/main/java/fiit/hipstery/publisher/bl/service/CollectionService.java
+++ src/main/java/fiit/hipstery/publisher/bl/service/CollectionService.java
@@ -8,11 +8,45 @@ import java.util.UUID;
 
 public interface CollectionService {
 
+	/**
+	 * Get all active collections
+	 */
 	Collection<CollectionDTO> getCollectionList();
+
+	/**
+	 * Get specific collection by id
+	 * @param id collection primary key
+	 * @param currentUser currently logged in user
+	 * @return dto containing collection data
+	 */
 	CollectionDTO getCollection(UUID id, UUID currentUser);
+
+	/**
+	 * Get collections created by this user
+	 * @param userId id of the user
+	 * @return list of collections created by this user
+	 */
 	Collection<CollectionDTO> getCollectionsForUser(UUID userId);
+
+	/**
+	 * Create a new collection
+	 * @param collectionInsertDTO dto containing data about the collection
+	 * @return id of the created collection
+	 */
 	UUID insertCollection(CollectionInsertDTO collectionInsertDTO);
+
+	/**
+	 * Assign article to a collection
+	 * @param collectionId id of the collection
+	 * @param articleId id of the article
+	 */
 	void assignArticle(UUID collectionId, UUID articleId);
+
+	/**
+	 * Saves changes to a collection
+	 * @param collection dto to be persisted
+	 * @return updated dto
+	 */
 	CollectionDTO updateCollection(CollectionDTO collection);
 
 }

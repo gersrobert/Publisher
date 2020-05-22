c31a5a5660749e356439bc53191e1c8013093788
Cupprum <branisa.samuel@windowslive.com>
2020-05-22 04:21:16 +0200

    export to pdf kindof works

--- src/main/java/fiit/hipstery/publisher/bl/impl/PdfServiceImpl.java
+++ src/main/java/fiit/hipstery/publisher/bl/impl/PdfServiceImpl.java
@@ -1,9 +1,13 @@
 package fiit.hipstery.publisher.bl.impl;
 
+import com.github.javafaker.App;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
 import fiit.hipstery.publisher.bl.service.PdfService;
+import fiit.hipstery.publisher.entity.AppUser;
 import fiit.hipstery.publisher.entity.Article;
+import fiit.hipstery.publisher.entity.Category;
+import fiit.hipstery.publisher.entity.Comment;
 import fiit.hipstery.publisher.repository.ArticleRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +18,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.format.DateTimeFormatter;
 import java.util.Base64;
 import java.util.UUID;
 
@@ -34,9 +39,68 @@ public class PdfServiceImpl implements PdfService {
 		PdfWriter.getInstance(document, stream);
 
 		document.open();
-		Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
-		Chunk chunk = new Chunk(article.getTitle(), font);
-		document.add(chunk);
+		Font metadataPrimaryFont = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+		Font metadataSecondaryFont = FontFactory.getFont(FontFactory.COURIER, 14, BaseColor.BLACK);
+		Font contentFont = FontFactory.getFont(FontFactory.COURIER, 12, BaseColor.BLACK);
+
+
+		Chunk title = new Chunk("Title : " + article.getTitle(), metadataPrimaryFont);
+		Phrase phTitle = new Phrase(title);
+
+		String categoryNames = new String();
+		for (Category category : article.getCategories()) {
+			if (categoryNames.isBlank()) {
+				categoryNames = category.getName();
+			} else {
+				categoryNames += ", " + category.getName();
+			}
+		}
+		Chunk categories = new Chunk("Categories : " + categoryNames, metadataSecondaryFont);
+		Phrase phCategories = new Phrase(categories);
+
+		Chunk publisher = new Chunk("Publisher : " + article.getPublisher().getName(), metadataSecondaryFont);
+		Phrase phPublisher = new Phrase(publisher);
+
+		Chunk createdAt = new Chunk("Published : " + DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm").format(article.getCreatedAt()), metadataSecondaryFont);
+		Phrase phCreatedAt = new Phrase(createdAt);
+
+		Chunk likes = new Chunk("Number Of Likes : " + String.valueOf(article.getLikeCount()), metadataSecondaryFont);
+		Phrase phLikes = new Phrase(likes);
+
+		Chunk content = new Chunk(article.getContent(), contentFont);
+		Phrase phContent = new Phrase(content);
+
+		Paragraph ph = new Paragraph();
+		ph.add(phTitle);
+		ph.add("\n");
+		ph.add("\n");
+		ph.add(phCategories);
+		ph.add("\n");
+		ph.add(phPublisher);
+		ph.add("\n");
+		ph.add(phCreatedAt);
+		ph.add("\n");
+		ph.add(phLikes);
+		ph.add("\n");
+		ph.add("\n");
+		ph.add(phContent);
+		ph.add("\n");
+		ph.add("\n");
+		Chunk commentsTitle = new Chunk("Comments", metadataSecondaryFont);
+		Phrase phCommentsTitle = new Phrase(commentsTitle);
+		ph.add(phCommentsTitle);
+		ph.add("\n");
+
+		for (Comment comment : article.getComments()) {
+			String commentAuthor = comment.getAuthor().getFirstName() + " " + comment.getAuthor().getLastName();
+			Chunk commentChunk = new Chunk(commentAuthor + " : " + comment.getContent(), contentFont);
+			Phrase phComment = new Phrase(commentChunk);
+			ph.add(phComment);
+			ph.add("\n");
+		}
+
+		document.add(ph);
+
 		document.close();
 
 		try {

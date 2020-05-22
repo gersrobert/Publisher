bbc68b693fc42f2f97fea1a8d1935071d1b45f64
Cupprum <branisa.samuel@windowslive.com>
2020-05-22 14:46:12 +0200

    comments on articleservice and pdfservice

--- src/main/java/fiit/hipstery/publisher/bl/service/PdfService.java
+++ src/main/java/fiit/hipstery/publisher/bl/service/PdfService.java
@@ -6,5 +6,10 @@ import java.io.IOException;
 import java.util.UUID;
 
 public interface PdfService {
+	/**
+	 * This method gets article from DB and returns it in PDF format
+	 * @param articleId is UUID of article, which user wants to export
+	 * @return Base64 encoded pdf
+	 */
 	String generatePdf(UUID articleId) throws DocumentException, IOException;
 }
